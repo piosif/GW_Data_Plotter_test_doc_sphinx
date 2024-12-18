6ca9e1bb23cabacda37740e38bb74dfe3a7cbe4c
Panagiotis Iosif <2871970+piosif@users.noreply.github.com>
2024-12-18 14:40:11 +0100

    remove text color from labels and tooltips

--- gw_data_plotter.py
+++ gw_data_plotter.py
@@ -18,7 +18,9 @@ from gwpy.timeseries import TimeSeries
 from gwosc.api import fetch_event_json, fetch_json
 
 
-from layout import Ui_MainWindow
+# from layout import Ui_MainWindow
+from layout_text_without_color import Ui_MainWindow
+
 
 
 import app_resources #PI: use a resources file (.qrc) to include images and fonts
